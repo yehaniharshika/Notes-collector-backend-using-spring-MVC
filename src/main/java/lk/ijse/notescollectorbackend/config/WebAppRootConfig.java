69907d15c8afe256609232696b18a00d913ed8b3
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-09-14 15:04:30 +0530

    create database

--- src/main/java/lk/ijse/notescollectorbackend/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/notescollectorbackend/config/WebAppRootConfig.java
@@ -1,9 +1,61 @@
 package lk.ijse.notescollectorbackend.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.notescollectorbackend")
+@EnableJpaRepositories
+@EnableTransactionManagement
+
 public class WebAppRootConfig {
+    //DataSource type ake dataSource method ekak meka ---> monawahari data related weda karnna awshya we
+    @Bean
+    public DataSource dataSource() {
+        /*in-memory database ekak thama methana thibbe*/
+        //EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        //return builder.setType(EmbeddedDatabaseType.HSQL).build();
+        var driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/notes_collector?createDatabaseIfNotExist=true");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("1234");
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        //Hibernate wala JPA wala implementation eka
+        //entityManagerFactory eken create wenne entityManagers la
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.acme.domain");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    //Transaction manager part eka(Transaction manage karanna)
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
 }
