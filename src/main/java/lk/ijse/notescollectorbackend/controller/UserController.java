609edf8022672bf7ad3bd0ec9602b9bdf28fe78f
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-09-30 09:09:38 +0530

    implement update user logics in UserController

--- src/main/java/lk/ijse/notescollectorbackend/controller/UserController.java
+++ src/main/java/lk/ijse/notescollectorbackend/controller/UserController.java
@@ -62,5 +62,38 @@ public class UserController {
         userService.deleteUser(userId);
         System.out.println("deleted");
     }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDTO> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PutMapping(value = "/{userId}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void updateUser(UserDTO userDTO,
+                           @RequestPart("firstName") String firstName,
+                           @RequestPart("lastName")String lastName,
+                           @RequestPart("email")String email,
+                           @RequestPart("password")String password,
+                           @RequestPart ("profilePic") MultipartFile profilePic,
+                           @PathVariable("userId") String userId) {
+
+        String base64ProPic = "";
+        try {
+            byte [] bytesProPic = profilePic.getBytes();
+            base64ProPic = AppUtil.profilePicToBase64(bytesProPic);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //TO Do: Build the object
+        var buildUserDTO = new UserDTO();
+        buildUserDTO.setUserId(userId);
+        buildUserDTO.setFirstName(firstName);
+        buildUserDTO.setLastName(lastName);
+        buildUserDTO.setEmail(email);
+        buildUserDTO.setPassword(password);
+        buildUserDTO.setProfilePic(base64ProPic);
+        userService.updateUser(userId,buildUserDTO);
+    }
 }
 
